28a8e92df6a314655bf204714d33a1e965ac3b61
tomohumim <tomofumi676@gmail.com>
2020-01-07 19:16:31 +0900

    メッセージ実装
    メッセージ実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,15 +14,15 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-    def update
-      if @group.update(group_params)
-        redirect_to root_path, notice: 'グループを更新しました'
-      else
-        render :edit
-      end
-    end
   end
   
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group) , notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
 
   private
   def group_params
