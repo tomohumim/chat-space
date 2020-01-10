9d87373d04d4b76d3b5955f2bcbbd1a0e8e78669
tomohumim <tomofumi676@gmail.com>
2020-01-10 19:53:39 +0900

    非同期通信

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -4,42 +4,41 @@ $(function(){
     if ( message.image ) {
       var html =
       `<div class="message"data-message-id= ${message.id}>
-                            <div class="upper-message">
-                            <div class="upper-message__user-name">
-                            ${message.user_name}
-                            </div>
-                            <div class="upper-message__date">
-                            ${message.created_at}
-                            </div>
-                            </div>
-                            <div class="lower-message">
-                            <p class="lower-message__content">
-                            ${message.content}
-                            </p>
-                            
-                            </div>
-                            <img src=${message.image}>
-                            </div>`
-                            return html;
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+          ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+          ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+          ${message.content}
+          </p>
+        </div>
+      <img src=${message.image}>
+     </div>`
+      return html;
   }else{
-    var html =
+  var html =
 `<div class="message"data-message-id= ${message.id}>
-  <div class="upper-message">
-  <div class="upper-message__user-name">
-  ${message.user_name}
-  </div>
-  <div class="upper-message__date">
-  ${message.created_at}
-  </div>
-  </div>
+    <div class="upper-message">
+      <div class="upper-message__user-name">
+      ${message.user_name}
+      </div>
+      <div class="upper-message__date">
+      ${message.created_at}
+      </div>
+   </div>
   <div class="lower-message">
-  <p class="lower-message__content">
-  ${message.content}
-  </p>
+    <p class="lower-message__content">
+    ${message.content}
+    </p>
   
   </div>
   
-  </div>`
+</div>`
   return html;
 
   };
