5ec36bb14c340a53477c3aaabcea2981fd3b1580
tomohumim <tomofumi676@gmail.com>
2020-01-10 20:09:07 +0900

    非同期通信

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -23,21 +23,21 @@ $(function(){
   }else{
   var html =
 `<div class="message"data-message-id= ${message.id}>
-    <div class="upper-message">
-      <div class="upper-message__user-name">
-      ${message.user_name}
-      </div>
-      <div class="upper-message__date">
-      ${message.created_at}
-      </div>
-   </div>
-  <div class="lower-message">
-    <p class="lower-message__content">
-    ${message.content}
-    </p>
-  
+  <div class="upper-message">
+    <div class="upper-message__user-name">
+    ${message.user_name}
+    </div>
+    <div class="upper-message__date">
+    ${message.created_at}
+    </div>
   </div>
-  
+  <div class="lower-message">
+  <p class="lower-message__content">
+  ${message.content}
+  </p>
+
+  </div>
+
 </div>`
   return html;
 
