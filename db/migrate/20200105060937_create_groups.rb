206a176139038ee1c5f736bfbe92d263fb6f5a5e
tomohumim <tomofumi676@gmail.com>
2020-01-15 17:21:33 +0900

    update

--- db/migrate/20200105060937_create_groups.rb
+++ db/migrate/20200105060937_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :nickname, null: false
-      t.index :nickname, unique: true
+      t.string :name, null: false
+      t.index :name, unique: true
       t.timestamps
     end
   end
